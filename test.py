7bda3ebe3e8e5d85340540a172355ddc0771b86c
KHANDELWAL Deepanshu <deepanshu.khandelwal@soprasteria.com>
2019-10-15 16:20:01 +0530

    done

--- test.py
+++ test.py
@@ -1,16 +1,19 @@
-import sqlite3
+import pyodbc
 import sys
 def fn(x,id,pref1,pref2):
     sql='insert into party1 values({},\'{}\',\'{}\',\'{}\');'.format(id,x,pref1,pref2)
     try:
-        with sqlite3.connect('SSI_Data.db', isolation_level=None) as conn:
-            print("Opened database successfully")
-            conn.execute('pragma journal_mode=wal')
-            print(sql)
-            cur1=conn.cursor()
-            cur1.execute(sql)
-            conn.commit()
+        conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=server_name;'
+                      'Database=db_name;'
+                      'Trusted_Connection=yes;')
+        conn.execute('pragma journal_mode=wal')
+        print(sql)
+        cur1=conn.cursor()
+        cur1.execute(sql)
+        conn.commit()
         return "Thank you"
     except:
         print('Error',sys.exc_info()[0])
         return "Error"
+ 
